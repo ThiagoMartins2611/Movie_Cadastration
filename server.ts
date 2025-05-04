22cf5fc606cc2340bbdbaef3c31c858e4d548cb4
ThiagoMartins2611 <thiagogames2611@gmail.com>
2025-05-04 16:45:42 -0400

    CadastrarUsers route finalizado

--- server.ts
+++ server.ts
@@ -19,13 +19,55 @@ app.get('/', async (request:FastifyRequest, reply:FastifyReply)=>{
 });
 
 
-app.post('/cadastrar', (request:FastifyRequest, reply:FastifyReply)=>{
+app.post('/cadastrarUsers', async (request:FastifyRequest, reply:FastifyReply)=>{
 
-    const {nome, email, senha} = request.body as any;
+    const {nome, email, senha} = request.body as {nome:string, email:string, senha:string};
 
     console.table(request.body);
 
-    
+    try {
+
+        const conn = await mysql.createConnection({
+            host: "localhost",
+            user: 'root',
+            password:"",
+            port: 3306
+        });
+
+
+        await conn.query("CREATE DATABASE IF NOT EXISTS MovieCritcs");
+        await conn.end();
+
+
+        const dbconn = await mysql.createConnection({
+            host: "localhost",
+            user: 'root',
+            password:"",
+            database:"MovieCritcs",
+            port: 3306
+        });
+
+        await dbconn.query(
+            `CREATE TABLE IF NOT EXISTS users(
+            
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                nome VARCHAR(100),
+                email VARCHAR(100),
+                senha VARCHAR(100)
+            
+            )
+            `);
+
+        await dbconn.query("INSERT INTO users (nome, email, senha) VALUES (?,?,?)", [nome, email, senha]);
+
+        reply.status(200).send("dados enviados para o banco");
+        console.log("mandado para o banco")
+
+    } 
+    catch (error:any) {
+        console.error("erro ao mandar para o banco")
+        reply.status(500).send("erro ao conectar ou mandar")
+    }
 
     reply.status(200).send("Dados recebidos");
 });
