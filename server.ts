7f6bdaf838d7f1523be7ddede194732d38fb708d
ThiagoMartins2611 <thiagogames2611@gmail.com>
2025-05-04 17:45:20 -0400

    loginConfig

--- server.ts
+++ server.ts
@@ -13,7 +13,6 @@ app.register(fastifyCookie);
 
 
 
-
 app.get('/', async (request:FastifyRequest, reply:FastifyReply)=>{
     reply.send("servidor rodando corretamente");
 });
@@ -52,7 +51,7 @@ app.post('/cadastrarUsers', async (request:FastifyRequest, reply:FastifyReply)=>
             
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 nome VARCHAR(100),
-                email VARCHAR(100),
+                email VARCHAR(100) UNIQUE,
                 senha VARCHAR(100)
             
             )
@@ -60,8 +59,11 @@ app.post('/cadastrarUsers', async (request:FastifyRequest, reply:FastifyReply)=>
 
         await dbconn.query("INSERT INTO users (nome, email, senha) VALUES (?,?,?)", [nome, email, senha]);
 
+      
         reply.status(200).send("dados enviados para o banco");
         console.log("mandado para o banco")
+
+        dbconn.end()
 
     } 
     catch (error:any) {
@@ -70,8 +72,68 @@ app.post('/cadastrarUsers', async (request:FastifyRequest, reply:FastifyReply)=>
     }
 
     reply.status(200).send("Dados recebidos");
+
+    
 });
 
+
+app.post('/LoginUser', async (request:FastifyRequest, reply:FastifyReply)=>{
+
+    const {email, senha} = request.body as {email:string, senha:string} 
+    console.table(request.body)
+    try {
+
+        const conn = await mysql.createConnection({
+            host: "localhost",
+            user: 'root',
+            password:"",
+            port: 3306
+        });
+
+
+        await conn.query("CREATE DATABASE IF NOT EXISTS MovieCritcs");
+        await conn.end();
+
+
+        const dbconn = await mysql.createConnection({
+            host: "localhost",
+            user: 'root',
+            password:"",
+            database:"MovieCritcs",
+            port: 3306
+        });
+
+
+        await dbconn.query(
+            `CREATE TABLE IF NOT EXISTS users(
+            
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                nome VARCHAR(100),
+                email VARCHAR(100) UNIQUE,
+                senha VARCHAR(100)
+            
+            )
+            `);
+
+       const [rows] = await dbconn.query("SELECT * FROM users WHERE email = ? AND senha = ?", [email, senha]); 
+       // a consulta que fazemos pelo mysql retorna uma array e um desses valores da array é "rows", no caso esse são os usuario que o progama encontra que tem aquelas infromações. 
+
+       const usuarios = rows as any[];
+       
+       if(usuarios.length == 0){
+        reply.status(401).send({mensagem: "Login Não cadastrado"});
+       }       
+      
+       reply.status(200).send("dados enviados para o banco");
+
+
+        
+    } catch (error:any) {
+        
+    }
+   
+
+});
 
 
 
